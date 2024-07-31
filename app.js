8a16497b3b213e834964c5d17ffd3c43dd6c41a0
marin-duv <marinz3us@gmail.com>
2024-07-31 13:49:04 -0600

    funcion onclick  app.js y index.html

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = 'Juego del número Secreto'
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Selecciona un número del 1 al 10'
+
+function intentoDeUsuario() {
+    alert ('click desde el boton')
+}
