79f39f00679be4b1e479afdcc58491959d7b31f1
marin-duv <marinz3us@gmail.com>
2024-07-31 13:28:13 -0600

    titulo y parrafo con querySelector app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del número Secreto'
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Selecciona un número del 1 al 10'
