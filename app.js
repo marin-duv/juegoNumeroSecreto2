d7234e4b03e1588ba05501fb58f9acd473eb6041
marin-duv <marinz3us@gmail.com>
2024-07-31 14:08:07 -0600

    funcion suma  app.js y index.html

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del número Secreto'
+titulo.innerHTML = 'Hora del desafio'
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Selecciona un número del 1 al 10'
 
 function intentoDeUsuario() {
-    alert ('click desde el boton')
+    alert ('click desde el boton');
+}
+
+function console() {
+    alert ('el boton fue clickeado');
+    let ciudad = prompt ('¿Nombre de la ciudad de Brasil?');
+    alert (`Estuve en ${ciudad} y me acorde de ti`);
+    alert (`Yo amo JS`);
+    let numero1 = parseInt(prompt (`Escribe el primer número`));
+    let numero2 = parseInt(prompt (`escribe el segundo número`));
+    suma = (numero1 + numero2);
+    alert (`Suma es igual a ${suma}`);
 }
