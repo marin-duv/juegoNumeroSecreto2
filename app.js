fa4ae03378014a650a2cc593a415a4427b59693e
marin-duv <marinz3us@gmail.com>
2024-08-03 00:39:45 -0600

    desafios b funciones, index.html, app.js

--- app.js
+++ app.js
@@ -86,6 +86,13 @@ function generarNumeroSecreto() {
 }
 
 condicionesIniciales();
+
+
+//AQUI ME QUEDE :
+//https://app.aluracursos.com/course/logica-programacion-explorar-funciones-listas/task/86064
+
+
+
 /*
 // se desabilitan por que se encapsularon en una funcion de mensajes inciales
 asignarTextoElemento('h1','Juego del Número Secreto ');
@@ -180,6 +187,114 @@ function multiplicar(numeroA){
 }
 multiplicar(9)
 */
+
+
+
+/*
+
+//DESAFIOS_B
+
+//1-Crea una función que calcule el índice de masa corporal (IMC) de una persona a partir de su altura en metros y peso en kilogramos, que se recibirán como parámetros.
+
+function masaCorporal(altura, peso) {
+imc = ((peso) / (altura * altura));
+console.log('imc= ',(imc));
+return;
+}
+masaCorporal(1.74 , 85);
+
+
+//IMC RESULTADO ES: 28.075
+
+//IMC 	Nivel de peso
+//Por debajo de 18.5 	Bajo peso
+//18.5—24.9 	Peso saludable
+//25.0—29.9 	Sobrepeso
+//30.0 o más 	Obesidad
+
+
+//2-Crea una función que calcule el valor del factorial de un número pasado como parámetro.
+
+//Bibliografia: 
+//Explicacion del factorial liga de internet: 
+//https://factorial.mx/numero-funcion-factorial
+
+function factorial(factor) {
+    if (factor < 0) {
+        return ("El Factorial no puede ser negativo");
+    }
+    if (factor === 0 || factor === 1) {
+        return 1;
+    }
+    let resultado = 1;
+    for (let i = 2; i <= factor; i++){
+        //console.log(i);
+        resultado *= i;
+        //console.log(i);
+        //console.log(resultado);
+    }
+    return resultado;
+}
+console.log(('El Factorial es ='),(factorial(4)));
+//console.log(('El Factorial es ='),(factorial(0)));
+//console.log(('El Factorial es ='),(factorial(-3)));
+
+
+//funcion dolares a pesos 
+//Crea una función que convierta un valor en dólares, pasado como parámetro, y devuelva el 
+//valor equivalente en reales(moneda brasileña,si deseas puedes hacerlo con el valor del
+//dólar en tu país). Para esto, considera la cotización del dólar igual a R$4,80.
+
+function conversor (pesos, dolar){
+    return  (pesos * dolar);
+    //el valor del dolar se debe conseguir de una api, para tener actualizada la conversion
+}
+console.log ("Convertir Dolar a Peso = ", (conversor(1600, 19.18)));
+//sueldo de programador que debo buscar 
+
+
+//Crea una función que muestre en pantalla el área y el perímetro de una sala rectangular,
+//utilizando la altura y la anchura que se proporcionarán como parámetros
+
+function rectangulo(alto, ancho){
+    area = (alto * ancho);
+    alert(`Èl area es ${area}`);
+    perimetro = (alto * ancho) * (2);
+    alert(`Èl perimetro es ${perimetro}`);
+    return;
+}
+rectangulo(10,20);
+
+
+//Crea una función que muestre en pantalla el área y el perímetro de una sala circular, 
+//utilizando su radio que se proporcionará como parámetro. Considera Pi = 3,14.
+
+function circulo(radio ){
+    area = (3.14) * (radio * radio );
+    alert (`area de circulo es = ${area}`); 
+    perimetro = (3.14) * (radio + radio);
+    alert (`perimetro de circulo es = ${perimetro}`); 
+    return;
+}
+circulo(8);
+
+//Crea una función que muestre en pantalla la tabla de multiplicar de un número dado como parámetro.
+
+function multiplica(numero){
+    for (i=0; i<=10; i++){
+        resultado = i * numero;
+        console.log(`${numero} x ${i} = ${resultado}`)
+    }
+}
+multiplica(8);
+
+
+//FIN DE DESAFIOS_B
+*/
+
+
+
+
 
 /*
 // solo use un boton para realizar el reto de ejercicios 
